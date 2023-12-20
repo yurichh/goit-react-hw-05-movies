d0abd7ed849c4941227c3beccaa58d5b7b157699
unknown <velidchukaleksey@gmail.com>
2023-12-20 23:47:00 +0200

    failed start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,23 @@
-export const App = () => {
-  return <div>AHAHHAHA</div>;
-};
+import { NavLink, Route, Routes } from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <ul>
+      <li>
+        <NavLink to="/"> Home page </NavLink>
+      </li>
+      <li>
+        <NavLink to="/dogs"> Dogs </NavLink>
+      </li>
+    </ul>
+
+    <Routes>
+      <Route path="/" element={<div>its a header</div>}>
+        <Route index element={<div> home page</div>} />
+        <Route path="/dogs" element={<div> dog`s page</div>} />
+        <Route path="*" element={<div>no page</div>} />
+      </Route>
+    </Routes>
+  </div>
+);
+export default App;
